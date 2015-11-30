a91234a24b5ae877ad44b5624486ab8b937726b6
Andrea Cognini <andrea.cognini@triboo.it>
2015-11-30 17:06:30 +0100

    new assets generated for gulp example

---
+++ dist/public_html/asset/script/script.js
@@ -0,0 +1,14 @@
+/**
+ * VENDOR
+ * @description Add short description about content
+ * @see http://
+ */
+var vendorExample;
+
+/**
+ * SCRIPTS
+ * Include here all core scripts
+ */
+var script;
+
+//# sourceMappingURL=sourcemaps/script.js.map
