59d41b8f04a387f1b770e7abae350e1533a03594
Andrea Cognini <andrea.cognini@triboo.it>
2015-11-30 17:02:42 +0100

    remove grunt files

--- dist/resource/html/require/head.req.php
+++ dist/resource/html/require/head.req.php
@@ -10,6 +10,8 @@
 <!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml" lang="it">
     <head>
+        <link rel="stylesheet" type="text/css" href="<?= CSS_PATH ?>style.css">
+
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
