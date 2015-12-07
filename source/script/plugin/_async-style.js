b1a1ce66bd132b21fbb70d1fe0d0a12c3f712596
Andrea Cognini <info@heavybeard.it>
2015-12-07 02:29:42 +0100

    minor correction asyncstyle

--- source/script/plugin/_async-style.js
+++ source/script/plugin/_async-style.js
@@ -2,12 +2,13 @@
  * ASYNC STYLE
  * @description Add "media" attribute to selected tag relative to "data-media". Default is "all"
  * @see http://keithclark.co.uk/articles/loading-css-without-blocking-render/
- * @param DOM el link[rel="stylesheet"] to download
+ * @param DOM element link[rel="stylesheet"] to download
  */
 
 var asyncStyle = function (element) {
     'use strict';
     
+    /** Set media to all */
     if (element.media === 'none') {
         element.media = element.getAttribute('data-media') || 'all';
     }
