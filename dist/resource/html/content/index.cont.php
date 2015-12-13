9ae69bf62a4529abb18a5cf56cb246c21b53d2ea
Andrea Cognini <info@heavybeard.it>
2015-12-13 18:48:21 +0100

    remove zoom on videojs player

--- dist/resource/html/content/index.cont.php
+++ dist/resource/html/content/index.cont.php
@@ -31,8 +31,8 @@
 <p>Gallia est omnis divisa in partes tres, quarum unam incolunt Belgae, aliam Aquitani, tertiam qui ipsorum lingua Celtae, nostra Galli appellantur. Hi omnes lingua, institutis, legibus inter se differunt. Gallos ab Aquitanis Garumna flumen, a Belgis Matrona et Sequana dividit. Horum omnium fortissimi sunt Belgae, propterea quod a cultu atque humanitate provinciae longissime absunt, minimeque ad eos mercatores saepe commeant atque ea quae ad effeminandos animos pertinent important, proximique sunt Germanis, qui trans Rhenum incolunt, quibuscum continenter bellum gerunt. Qua de causa Helvetii quoque reliquos Gallos virtute praecedunt, quod fere cotidianis proeliis cum Germanis contendunt, cum aut suis finibus eos prohibent aut ipsi in eorum finibus bellum gerunt. Eorum una, pars, quam Gallos obtinere dictum est, initium capit a flumine Rhodano, continetur Garumna flumine, Oceano, finibus Belgarum, attingit etiam ab Sequanis et Helvetiis flumen Rhenum, vergit ad septentriones. Belgae ab extremis Galliae finibus oriuntur, pertinent ad inferiorem partem fluminis Rheni, spectant in septentrionem et orientem solem. Aquitania a Garumna flumine ad Pyrenaeos montes et eam partem Oceani quae est ad Hispaniam pertinet; spectat inter occasum solis et septentriones.</p>
 <?php picture_wlz(IMG_PATH, 'example', array('alt' => 'Example text 2')); ?>
 
-<?php /* <video class="video-js" controls preload="none" poster="<?= VIDEO_PATH ?>example/example.jpg" data-setup width="640" data-action="zoom">
+<video class="video-js" controls preload="none" poster="<?= VIDEO_PATH ?>example/example.jpg" data-setup width="640">
     <source src="<?= VIDEO_PATH ?>example/example.webm" type="video/webm">
     <source src="<?= VIDEO_PATH ?>example/example.ogv" type="video/ogg"> 
     <source src="<?= VIDEO_PATH ?>example/example.mp4" type="video/mp4">
-</video> */ ?>
+</video>
